ed44348a917344920dcfa1d786ad426062e33778
shiminyiblaise <shiminyiblaise650@gmail.com>
2025-06-15 18:04:20 +0400

    linters test

--- tests_mylist.rb
+++ tests_mylist.rb
@@ -13,4 +13,5 @@ puts list.any? { |e| e == 2 } # => true
 puts list.any? { |e| e == 5 } # => false
 
 # Test #filter
+puts list.filter { |e| e > 2 } # => [3, 4]
 puts list.filter { |e| e.even? } # => [2, 4]
